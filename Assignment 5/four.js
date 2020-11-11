cd3e85751620e25b659a78e77501a8ff5db9e3eb
ShayanYaseen <shayan.yaseen1@gmail.com>
2020-11-12 00:31:57 +0530

    Updated Assignment 5

--- Assignment 5/four.js
+++ Assignment 5/four.js
@@ -1,5 +1,5 @@
 //Logic for printing tree
-function christmasTree(height_of_tree){
+function christmasTreelocal(height_of_tree){
     for (let i = 0; i < height_of_tree; i++) { 
         let str = ''; 
         for (let j = 1; j < height_of_tree-i; j++){ 
@@ -13,7 +13,26 @@ function christmasTree(height_of_tree){
     } 
 }
 
+function christmasTree(){
+    var height_of_tree = document.getElementById("string").value;
+    let finaltree = "";
+    for (let i = 0; i < height_of_tree; i++) { 
+        let str = ''; 
+        for (let j = 1; j < height_of_tree-i; j++){ 
+            str = str + "&nbsp" + "&nbsp"; 
+        } 
+        for (let k = 1; k <= (2*i+1); k++){ 
+            if(i == 0) str = str + '*';
+            else str = str + '0'; 
+        } 
+        finaltree += str +"<BR>";
+    } 
+    //var h1 = document.createElement('h1');
+    document.getElementById("p").innerHTML = finaltree;
+    //document.body.appendChild(h1);
+}
+
+// Run using node
+
 let height_of_tree = 10;
-
-
-christmasTree(height_of_tree)
+christmasTreelocal(height_of_tree)
--- Assignment 5/two.java
+++ Assignment 5/two.java
@@ -1,10 +1,12 @@
-class Threaded2 extends Thread{
+public class two extends Thread{
 
     private int range1;
     private int range2;
 
+    static int answer = 0,number=0;
+
     //Set range
-    public Threaded2(int range1,int range2){
+    public two(int range1,int range2){
         this.range1 = range1;
         this.range2 = range2;
     }
@@ -25,31 +27,47 @@ class Threaded2 extends Thread{
     public void run(){
         int most_divisors = 0;
         int ans = 1;
-        for(int i=1;i<=range1;i++){
+        for(int i=range1;i<=range2;i++){
             int current_divisors = countDivisors(i);
             if(current_divisors>most_divisors){
                 ans = i;
                 most_divisors = current_divisors;
             }
         }
-        System.out.println("The number in the range with most divisors is " + ans + "\nIt has divisors: "+ most_divisors);
+        if(most_divisors > number){
+            synchronized(this){
+                answer = ans;
+                number = most_divisors;
+            }
+        }
     }
-}
 
-public class two{
     public static void main(String[] args){ 
+        long start = System.currentTimeMillis();
+        int range1 = 1; 
+        int range2 = 5000; 
 
-        int range1 = 10000; 
-        int range2 = 10000; 
+        two thread_object1 = new two(range1,range2);
 
-        Threaded2 thread_object = new Threaded2(range1,range2);
+        range1 = 5001; 
+        range2 = 10000; 
 
-        thread_object.start();
-        
+        two thread_object2 = new two(range1,range2);
+
+
+        thread_object1.start();
+        thread_object2.start();
+
         try{
-            thread_object.join();
+            thread_object1.join();
+            thread_object2.join();
         }catch(InterruptedException excptn){
             System.out.println(excptn);
         }
+
+        System.out.println("The nubmer with most divisors is: " + answer + " with " + number + " divisiors");
+        long end = System.currentTimeMillis();
+        System.out.println("Caclulated with two threads in "+ (end-start)+ " time");
+
     }
 }
